8896096cd43d5881a3afe6673c42d3d4aa5d9b4b
Pedro Axelrud <pedroaxl@gmail.com>
2010-07-01 16:47:24 -0400

    instead of using Eventmachine::FileWatch we are using EventMachine.popen and using tail to get the latest additions to the log

--- src/handler.rb
+++ src/handler.rb
@@ -1,28 +1,8 @@
 module Handler
   require 'strscan'
 
-  def initialize
-    @position = 0
-  end
-  
-  def file_modified 
-    f = File.new(path,"r")
-    f.seek(@position, IO::SEEK_CUR) # rescue @position = 0
-    s = StringScanner.new f.read
-    trigger_callbacks s, config(path)
-    f.close
-  end
-  
-  def file_moved
-    puts "#{path} moved"    
-  end
-  
-  def file_deleted
-    puts "#{path} deleted"    
-  end
-  
-  def unbind
-    puts "#{path} monitoring ceased"    
+  def receive_data data
+    puts data
   end
   
   def config(path)
--- src/watcher.rb
+++ src/watcher.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'yaml'
 require 'eventmachine'
-require 'handler'
+require 'src/handler'
 
 config_file = YAML.load_file('config.yml')
 
 
 EM.run {
-  config_file.each do |key|
-    EM.watch_file(config_file[key]["path"], Handler)
+  config_file.each do |key,value|
+    EM.popen("tail -f #{config_file[key]["path"]}", Handler)
   end
 }
